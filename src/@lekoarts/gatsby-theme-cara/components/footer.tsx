34995f6e342d2f986ce87f010797fe748a2f70d3
Patel, Chirag (cp121p) <cp121p@att.com>
2020-06-11 11:46:15 -0400

    resolution added

--- src/@lekoarts/gatsby-theme-cara/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -3,22 +3,22 @@ import { Box, Flex, Link, useColorMode,  ColorMode, ThemeProvider,jsx } from "th
 import { ColorModeProvider } from '@theme-ui/color-modes'
 
 const Footer = () => {
-  //const [colorMode, setColorMode] = useColorMode()
-  // const isDark = colorMode === `dark`
-  // const toggleColorMode = (e: any) => {
-  //   setColorMode(isDark ? `light` : `dark`)
-  // }
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
 
   return (
     <Box as="footer" variant="footer">
-      {/* <button
+      <button
         sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
         onClick={toggleColorMode}
         type="button"
         aria-label="Toggle dark mode"
       >
         {isDark ? `Light` : `Dark`}
-      </button> */}
+      </button>
       Copyright &copy; {new Date().getFullYear()} Chirag Patel. All rights reserved.
       <br />
       <Flex
